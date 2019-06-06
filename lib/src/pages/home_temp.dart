94434c4f5e0992651c6cba64e332439eb46fed4a
Carlos García Reyes <carlossas_97@hotmail.com>
2019-06-06 17:25:24 -0500

    Checkpoint

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Componentes Temp")),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text("ListTitle"),
+            ),
+            Divider(),
+          ],
+        ));
+  }
+}
